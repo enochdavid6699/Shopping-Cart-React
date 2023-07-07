d7b69e6bab8e2d9e9cf0812bc4d542ea611f6c8f
Enoch <enochdavid6699@gmail.com>
2023-07-07 15:40:46 +0530

    Plus Minus Sign Action

--- src/cartItem.js
+++ src/cartItem.js
@@ -9,7 +9,37 @@ class CartItem extends React.Component {
       qty: 1,
       img: ''
     }
+    // Another way of binding
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+
+  //Easiest way of binding is through arrow function as it will bind automatically
+  increaseQuantity = () => {
+    // console.log(this);
+
+    //Set state form 1
+    this.setState({
+      qty: this.state.qty+1
+    })
+
+    //Set state form 2
+    // this.setState((prevState)=>{
+    //   return {
+    //     qty: prevState.qty+1
+    //   }
+    // })
+  }
+
+  decreaseQuantity = () => {
+
+    if(this.state.qty==1){
+      return;
+    }
+    this.setState({
+      qty: this.state.qty-1
+    })
+  }
+
   render () {
     const { price, title, qty } = this.state;
     return (
@@ -23,8 +53,21 @@ class CartItem extends React.Component {
           <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
-            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" />
+            <img 
+              alt="increase" 
+              className="action-icons" 
+              src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
+
+              //One way of binding
+              // onClick={this.increaseQuantity.bind(this)}
+              onClick={this.increaseQuantity}
+            />
+            <img 
+              alt="decrease" 
+              className="action-icons" 
+              src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+              
+              onClick={this.decreaseQuantity} />
             <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
           </div>
         </div>
